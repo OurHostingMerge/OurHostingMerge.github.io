ef284ce4b800eb312f3aff54d4e213829ea0c5ec
nicoperez12 <nicolas.perezp@live.cl>
2023-02-02 14:51:47 -0300

    arreglo imagenes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
                         <CardItem
-                            src='images/portada_mandala.png'
+                            src= {require('../images/portada_mandala.png')}
                             text='Paint mandalas with your favorite colors'
                             label='To Relax'
                             path='/mandalas'
                         />
                         <CardItem
-                            src='images/img-2.jpg'
+                            src={require('../images/img-2.jpg')}
                             text='Travel through the Islands of Bali in a Private Cruise'
                             label='Luxury'
                             path='/services'
@@ -24,19 +24,19 @@ function Cards() {
                     </ul>
                     <ul className='cards__items'>
                         <CardItem
-                            src='images/img-3.jpg'
+                            src={require('../images/img-3.jpg')}
                             text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
                             label='Mystery'
                             path='/services'
                         />
                         <CardItem
-                            src='images/img-4.jpg'
+                            src={require('../images/img-4.jpg')}
                             text='Experience Football on Top of the Himilayan Mountains'
                             label='Adventure'
                             path='/products'
                         />
                         <CardItem
-                            src='images/img-8.jpg'
+                            src={require('../images/img-8.jpg')}
                             text='Ride through the Sahara Desert on a guided camel tour'
                             label='Adrenaline'
                             path='/sign-up'
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import "./HeroSection.css"
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src='/videos/video-2.mp4' autoPlay loop muted />
+            <video src={require('../videos/video-2.mp4')} autoPlay loop muted />
             <h1 style={{ color: 'white' }}> TORY DIOS </h1>
             <p style={{ color: 'white' }}> El tory es un dios</p>
             <div className="hero-btns">
